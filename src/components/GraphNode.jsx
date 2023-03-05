3f301b7e586eafbf06a8f18c47ad77dffe40f55f
iguiIllanes <igui10082002@gmail.com>
2023-03-05 19:01:07 -0400

    temporary commit

--- src/components/GraphNode.jsx
+++ src/components/GraphNode.jsx
@@ -1,12 +1,31 @@
 import React, { memo } from "react";
 import { Handle } from "reactflow";
 
-export default memo(({ data, isConnectable, sourcePosition }) => {
+export default memo(({ handleId, data, isConnectable, sourcePosition }) => {
   return (
     <>
       <Handle
+        id={`${handleId}-left`}
         type="source"
-        position={sourcePosition}
+        position="left"
+        isConnectable={isConnectable}
+      />
+      <Handle
+        id={`${handleId}-right`}
+        type="source"
+        position="right"
+        isConnectable={isConnectable}
+      />
+      <Handle
+        id={`${handleId}-top`}
+        type="source"
+        position="top"
+        isConnectable={isConnectable}
+      />
+      <Handle
+        id={`${handleId}-bottom`}
+        type="source"
+        position="bottom"
         isConnectable={isConnectable}
       />
     </>
