18597e4356d61b01faa9a779b0f14808c8223bf6
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-03-05 19:38:11 -0400

    Custom node style

--- src/components/GraphNode.jsx
+++ src/components/GraphNode.jsx
@@ -9,26 +9,36 @@ export default memo(({ handleId, data, isConnectable, sourcePosition }) => {
         id={`${handleId}-left`}
         type="source"
         position="left"
+        style={{ background: "#555" }}
         isConnectable={isConnectable}
+        className="ring-handle"
       />
       <Handle
         id={`${handleId}-right`}
         type="source"
         position="right"
+        style={{ background: "#555" }}
+
         isConnectable={isConnectable}
       />
+      <div className="customNode">{data.label}</div>
       <Handle
         id={`${handleId}-top`}
         type="source"
         position="top"
+        style={{ background: "#555" }}
+
         isConnectable={isConnectable}
       />
       <Handle
         id={`${handleId}-bottom`}
         type="source"
         position="bottom"
+        style={{ background: "#555" }}
+
         isConnectable={isConnectable}
       />
+      {/* <div className="node-label">{data.label}</div> */}
     </>
   );
 });
