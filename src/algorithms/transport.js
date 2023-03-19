2eed494b5771c029b928c705c677af24e85fbce6
Chris Apaza <chrisalanapazaa@gmail.com>
2023-03-19 00:59:24 -0400

    implemented transport for first iteration

--- src/algorithms/transport.js
+++ src/algorithms/transport.js
@@ -1,5 +1,5 @@
 const data = {
-    "algorithm": "transport-min",
+    "algorithm": "transport-max",
     "numRows": 3,
     "numColumns": 4,
     "matrix": [
@@ -20,12 +20,10 @@ const demand = Array.from(matrix[numRows + 1].slice(1, numColumns + 1), val => p
 const cost = Array.from(matrix.slice(1, numRows + 1), row => Array.from(row.slice(1, numColumns + 1), val => parseInt(val)));
 
 // Create allocation matrix using Array.fill()
-const allocation = Array.from({ length: numRows }, () => Array.from({ length: numColumns }, () => 0));
+const allocationMatrix = Array.from({ length: numRows }, () => Array.from({ length: numColumns }, () => 0));
 
-// Initialize the total cost and the comparison function
-let totalCost = 0;
+// Initialize the comparison function
 let compareFunc;
-
 // Determine the comparison function based on the algorithm specified in the data object
 if (algorithm === "transport-min") {
     compareFunc = (a, b) => a - b;
@@ -33,24 +31,36 @@ if (algorithm === "transport-min") {
     compareFunc = (a, b) => b - a;
 }
 
-// Execute the Northwest Corner Method
+// Implement the northwest corner algorithm
 let i = 0;
 let j = 0;
 while (i < numRows && j < numColumns) {
     if (supply[i] < demand[j]) {
-        allocation[i][j] = supply[i];
+        allocationMatrix[i][j] = supply[i];
         demand[j] -= supply[i];
-        totalCost += cost[i][j] * supply[i];
         i++;
     } else {
-        allocation[i][j] = demand[j];
+        allocationMatrix[i][j] = demand[j];
         supply[i] -= demand[j];
-        totalCost += cost[i][j] * demand[j];
         j++;
     }
 }
 
+console.log("Allocation matrix:");
+console.table(allocationMatrix);
+
+
+
+
 // Print the results
-console.log("Allocation:");
-console.table(allocation);
+console.log("Allocation matrix:");
+console.table(allocationMatrix);
 console.log("Total cost:", totalCost);
+
+function swapColumns(arr, c1, c2) {
+    for (let i = 0; i < arr.length; i++) {
+        const temp = arr[i][c1];
+        arr[i][c1] = arr[i][c2];
+        arr[i][c2] = temp;
+    }
+}
