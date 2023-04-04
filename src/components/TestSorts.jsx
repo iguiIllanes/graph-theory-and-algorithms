5d741df10bff062c12674590c0c2166eb7e75dac
unknown <alejandro.flores.a@ucb.edu.bo>
2023-04-03 20:11:58 -0400

    SORTS changed

--- src/components/TestSorts.jsx
+++ src/components/TestSorts.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import { useState } from "react";
 import { generarListaAleatoria, insertionSort } from "../helpers/sorts_2";
+import { arrayToString, generateRandomArray, selectionSort } from "../algorithms/sorts";
 
 const TestSort = () => {
   const [selectedOption, setSelectedOption] = React.useState("");
   const [array, setArray] = useState([]);
+  const [isRandom, setIsRandom] = React.useState(false);
+  const [text, setText] = useState("");
+
+  
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+    
+    setArray(text);
+    console.log(array)
+  };
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
   };
   const handleRandomArray = () => {
-      var randomArray = generarListaAleatoria();
-      setArray(randomArray);
-      
+      // var randomArray = generarListaAleatoria();
+      // setArray(randomArray);
+      // console.log("Arreglo aleatorio",randomArray);
+
+
+
+      const randomArray = generateRandomArray();
       console.log("Arreglo aleatorio",randomArray);
+      setIsRandom(!isRandom);
+      const randomString = arrayToString(randomArray);
+      const randomArrayFromString = randomString.split('|');
+      setArray(randomArrayFromString);
    }
 const handleInsertionSort = () => {
 
@@ -30,6 +49,18 @@ const handleSelectionSort = () => {
   return (
     <div>
       <h2>Algoritmos de Ordenamiento</h2>
+
+      <div>
+
+        <textarea
+          rows="1"
+          value={isRandom ? array : text}
+          onChange={handleTextChange}
+          style={{ height: "auto", overflow: "hidden" }}
+        />
+        <h3>Input Field</h3>
+        <input type="text" placeholder={isRandom ? array : ''}/>
+      </div>
       <div>
         <button onClick={handleRandomArray} >Random</button>
       </div>
@@ -44,10 +75,7 @@ const handleSelectionSort = () => {
         <h3>Button 4</h3>
         <button>Button 4</button>
       </div>
-      <div>
-        <h3>Input Field</h3>
-        <input type="text" placeholder="Enter text here" />
-      </div>
+
       <div>
         <h3>Radio Buttons</h3>
         <label>
