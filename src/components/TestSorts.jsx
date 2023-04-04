4c23dfc13704ac57440c8f20bd5409ad2ac41bf5
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-04-03 20:11:33 -0400

    algortimos por consola

--- src/components/TestSorts.jsx
+++ src/components/TestSorts.jsx
@@ -1,48 +1,66 @@
-import React from "react";
-import { useState } from "react";
-import { generarListaAleatoria, insertionSort } from "../helpers/sorts_2";
+import React, { useState } from "react";
+import { generarListaAleatoria, insertionSort, selectionSort, mergeSort, shellSort } from "../helpers/sorts_2";
 
 const TestSort = () => {
-  const [selectedOption, setSelectedOption] = React.useState("");
+  const [selectedOption, setSelectedOption] = useState("");
   const [array, setArray] = useState([]);
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
   };
-  const handleRandomArray = () => {
-      var randomArray = generarListaAleatoria();
-      setArray(randomArray);
-      
-      console.log("Arreglo aleatorio",randomArray);
-   }
-const handleInsertionSort = () => {
 
-      let sortedArrayObject = insertionSort(array);
-      console.log("Arreglo ordenado - Insertion",sortedArrayObject.sortedArray);
-      console.log("Operaciones - Insertion",sortedArrayObject.numOperations);
-      console.log("Runtime - Insertion",sortedArrayObject.runtime);
-}
-const handleSelectionSort = () => {
-      let sortedArray = selectionSort(array);
-      console.log("Arreglo ordenado - Selection",sortedArray);
-}
+  const handleRandomArray = () => {
+    const randomArray = generarListaAleatoria();
+    setArray(randomArray);
+
+    console.log("Arreglo aleatorio", randomArray);
+  };
+
+  const handleInsertionSort = () => {
+    const sortedArrayObject = insertionSort(array);
+    console.log("Arreglo ordenado - Insertion", sortedArrayObject.sortedArray);
+    console.log("Operaciones - Insertion", sortedArrayObject.numOperations);
+    console.log("Runtime - Insertion", sortedArrayObject.runtime);
+  };
+
+  const handleSelectionSort = () => {
+    const sortedArray = selectionSort(array);
+    console.log("Arreglo ordenado - Selection", sortedArray.sortedArray);
+    console.log("Operaciones - Selection", sortedArray.numOperations);
+    console.log("Runtime - Selection", sortedArray.runtime);
+  };
+
+  const handleShellSort = () => {
+    const sortedArray = shellSort(array);
+    console.log("Arreglo ordenado - Shell", sortedArray.sortedArray);
+    console.log("Operaciones - Shell", sortedArray.numOperations);
+    console.log("Runtime - Shell", sortedArray.runtime);
+  };
+
+  const handleMergeSort = () => {
+    const sortedArray = mergeSort(array);
+    console.log("Arreglo ordenado - Merge", sortedArray.sortedArray);
+    console.log("Operaciones - Merge", sortedArray.numSteps);
+    console.log("Runtime - Merge", sortedArray.runtime);
+  };
 
   return (
     <div>
       <h2>Algoritmos de Ordenamiento</h2>
       <div>
-        <button onClick={handleRandomArray} >Random</button>
+        <button onClick={handleRandomArray}>Random</button>
       </div>
       <div>
         <button onClick={handleInsertionSort}>Insertion Sort</button>
       </div>
       <div>
-        <h3>Button 3</h3>
-        <button>Button 3</button>
+        <button onClick={handleSelectionSort}>Selection Sort</button>
       </div>
       <div>
-        <h3>Button 4</h3>
-        <button>Button 4</button>
+        <button onClick={handleShellSort}>Shell Sort</button>
+      </div>
+      <div>
+        <button onClick={handleMergeSort}>Merge Sort</button>
       </div>
       <div>
         <h3>Input Field</h3>
